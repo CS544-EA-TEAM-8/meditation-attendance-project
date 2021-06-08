da431ad6b7a40a0523ad9c805c92064af45d1d20
Efrem Abrehe <efrem.berhane2009@gmail.com>
2021-06-08 15:40:38 -0500

    Add @Qualifier name

--- src/main/java/edu/ea/project/team8/service/FacultyServiceImpl.java
+++ src/main/java/edu/ea/project/team8/service/FacultyServiceImpl.java
@@ -3,14 +3,16 @@ package edu.ea.project.team8.service;
 import edu.ea.project.team8.domain.Course;
 import edu.ea.project.team8.repository.FacultyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
-public class FacultyServiceImpl implements FacultyService{
+public class FacultyServiceImpl implements FacultyService {
 
+    @Qualifier("facultyRepository")
     @Autowired
     FacultyRepository facultyRepository;
 
