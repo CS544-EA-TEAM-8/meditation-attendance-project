c134eccf2ed6aaa02c033917e84212214d77bfe6
Ghirmay-Shinash <goreshinash14@gmail.com>
2021-06-08 17:57:12 -0500

    Add CRUD for Location

--- src/main/java/edu/ea/project/team8/service/LocationService.java
+++ src/main/java/edu/ea/project/team8/service/LocationService.java
@@ -2,8 +2,13 @@ package edu.ea.project.team8.service;
 
 import java.util.List;
 
+import edu.ea.project.team8.domain.Faculty;
 import edu.ea.project.team8.domain.Location;
 
 public interface LocationService {
-	public List<Location> findAll(); 
+	List<Location> getAllLocation();
+	Location findLocationById(Integer id);
+	void addLocation(Location location);
+	Location updateLocation(Location location);
+	void deleteLocation (Integer id);
 }
