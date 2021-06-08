949a8b2fc8f87ba3c8cb5f4970d5824a4137dac0
Efrem Abrehe <efrem.berhane2009@gmail.com>
2021-06-08 15:39:53 -0500

    Delete unused imports

--- src/main/java/edu/ea/project/team8/controller/FacultyController.java
+++ src/main/java/edu/ea/project/team8/controller/FacultyController.java
@@ -1,26 +1,28 @@
 package edu.ea.project.team8.controller;
 
-import edu.ea.project.team8.service.CourseService;
+import edu.ea.project.team8.domain.Course;
 import edu.ea.project.team8.service.FacultyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class FacultyController {
 
-    @Autowired
-    FacultyService facultyService;
+//    @Autowired
+//    FacultyService facultyService;
 
-    @Autowired
-    CourseService courseService;
-
-//    @GetMapping("/faculty/")
-//    List<Course> courseList (Integer id){
-//        Faculty faculty = facultyService.;
-//        Course course = courseService.getCourseById(id);
-//        Faculty faculty = facultyService.getCourseNowTeaching(course.getId(), LocalDate.now());
+//    @GetMapping("/faculty/{id}/courses")
+//    Course getCourseFacultyTeaching(@PathVariable Integer id) {
+//        return facultyService.getCourseNowTeaching(id, LocalDate.now());
 //    }
 
-
-
+//    @GetMapping("/faculty/{id}/courseList")
+//    List<Course> getCourseFacultyTeachingSixMonth(@PathVariable Integer id) {
+//        return facultyService.courseFromSixMonths(id, LocalDate.now());
+//    }
 }
